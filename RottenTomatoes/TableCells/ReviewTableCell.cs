adce6fa300acb79a90392ba3f7e01f28ec86fd52
Pavel Alekseev <alekseev.pg@gmail.com>
2014-06-05 17:15:46 +0400

    Added ReviewTableCell

---
+++ RottenTomatoes/TableCells/ReviewTableCell.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using MonoTouch.UIKit;
+using System.Drawing;
+using RottenApi;
+using MonoTouch.Foundation;
+
+namespace RottenTomatoes.TableCells
+{
+    public class ReviewTableCell : UITableViewCell
+    {
+        public static readonly NSString CellId = new NSString("ReviewTableCell");
+        private UILabel _titleLbl, _reviewLbl;
+        private UIImageView _freshView;
+        private Review _review;
+
+        public NSUrl ReviewUrl
+        {
+            get
+            { 
+                if (_review != null && !string.IsNullOrEmpty(_review.Links.Review))
+                    return new NSUrl(_review.Links.Review);
+                return null;
+            }
+        }
+
+        public ReviewTableCell(IntPtr handle)
+            : base(handle)
+        {
+            SelectionStyle = UITableViewCellSelectionStyle.None;
+            Accessory = UITableViewCellAccessory.DisclosureIndicator;
+            _freshView = new UIImageView(new RectangleF(5, 20, 15, 15));
+            Add(_freshView);
+
+
+            _titleLbl = new UILabel(new RectangleF(25, 5, 270, 20))
+            {
+                Font = UIFont.FromName("HelveticaNeue-italic", 13),
+                TextColor = UIColor.Black,
+            };
+            Add(_titleLbl);
+
+            _reviewLbl = new UILabel(new RectangleF(25, 25, 270, 20))
+            {
+                Font = UIFont.FromName("HelveticaNeue", 13),
+                TextColor = UIColor.Black,
+            };
+            Add(_reviewLbl);
+        }
+
+        public void UpdateReview(Review review)
+        {
+            _review = review;
+            switch (review.Freshness)
+            {
+                case "certified fresh":
+                    _freshView.Image = Images.Get("Content/CF_300x300.png");
+                    break;
+                case "fresh":
+                    _freshView.Image = Images.Get("Content/fresh.png");
+                    break;
+                case "rotten":
+                    _freshView.Image = Images.Get("Content/rotten.png");
+                    break;
+                default:
+                    _freshView.Image = null;
+                    break;
+            }
+            _titleLbl.Text = string.Format("{0}, {1}", review.Critic, review.Publication);
+            _reviewLbl.Text = review.Quote;
+        }
+    }
+}
+
