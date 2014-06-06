a365daa890ad361db4cb90cd4743cbc5d272c6e8
Pavel Alekseev <alekseev.pg@gmail.com>
2014-06-06 19:46:24 +0400

    Added calabash script

--- features/step_definitions/my_first_steps.rb
+++ features/step_definitions/my_first_steps.rb
@@ -1,4 +1,10 @@
 Given /^I am on the Welcome Screen$/ do
-  element_exists("view")
-  sleep(STEP_PAUSE)
+
+	wait_for(:timeout => 5) { element_exists("label text:'Top Box Office'") }
+	touch("view marked: 'MovieCell-1-1'")
+
+	wait_for(:timeout => 5) { element_exists("label text:'Movie info'") }
+	# check_element_exists("view marked: 'Rotten'")
+	check_element_does_not_exist("view marked: 'Rotten'")
+	sleep(STEP_PAUSE)
 end
